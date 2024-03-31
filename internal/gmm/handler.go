fef94bdd77aee0f1e220205538d2c2079f296a4a
tim1207 <timlin891207@gmail.com>
2024-03-31 06:57:50 +0000

    edit go.mod

--- internal/gmm/handler.go
+++ internal/gmm/handler.go
@@ -33,7 +33,8 @@ import (
 	"github.com/free5gc/ngap/ngapConvert"
 	"github.com/free5gc/ngap/ngapType"
 	"github.com/free5gc/openapi"
-	"github.com/free5gc/openapi/Nnrf_NFDiscovery"
+	//"github.com/free5gc/openapi/Nnrf_NFDiscovery"
+	"github.com/free5gc/openapi/nrf/NFDiscovery"
 	"github.com/free5gc/openapi/models"
 	"github.com/free5gc/util/fsm"
 )
